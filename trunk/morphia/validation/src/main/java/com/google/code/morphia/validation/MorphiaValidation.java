f9022472c68bca6c0cdbbd6557204df372079716
jendave.hudson@gmail.com <jendave.hudson@gmail.com@a4094458-e237-ed74-87d6-2fa648c436f9>
2012-02-24 03:54:06 +0000

    initial checkin of morphia

---
+++ trunk/morphia/validation/src/main/java/com/google/code/morphia/validation/MorphiaValidation.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.google.code.morphia.validation;
+
+@Deprecated
+/**
+ * @deprecated renamed to ValidationExtension
+ */
+public class MorphiaValidation extends ValidationExtension
+{
+}
