dc7e1c596a7685893b041ba911145629ddec8c6c
jendave.hudson@gmail.com <jendave.hudson@gmail.com@a4094458-e237-ed74-87d6-2fa648c436f9>
2012-10-12 23:13:44 +0000

    move package

--- trunk/proxy-printer/src/main/java/com/googlecode/arcane/proxy/ProxyPrinterRunner.java
+++ trunk/proxy-printer/src/main/java/com/googlecode/arcane/proxy/ProxyPrinterRunner.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.googlecode.arcane.proxy.proxy;
+package com.googlecode.arcane.proxy;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
