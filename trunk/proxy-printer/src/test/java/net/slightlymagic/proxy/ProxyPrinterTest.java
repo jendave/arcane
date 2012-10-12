b097e65f38d185f9f0f08838e11bc6108571f594
jendave.hudson@gmail.com <jendave.hudson@gmail.com@a4094458-e237-ed74-87d6-2fa648c436f9>
2012-10-12 19:59:51 +0000

    initial checkin of proxy printer

---
+++ trunk/proxy-printer/src/test/java/net/slightlymagic/proxy/ProxyPrinterTest.java
@@ -0,0 +1,38 @@
+package net.slightlymagic.proxy;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class ProxyPrinterTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName deckName of the test case
+     */
+    public ProxyPrinterTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( ProxyPrinterTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
